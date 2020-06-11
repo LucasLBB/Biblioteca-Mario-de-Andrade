03097e43404698460f8048e3a89db47064924346
Lucas de Lima <lucas2001brito@hotmail.com>
2020-06-11 20:53:49 -0300

    Alteração

--- backend/altDadosCat.php
+++ backend/altDadosCat.php
@@ -5,6 +5,21 @@ require_once "../conexao/conexao.php";
 $idCat = $_POST['idCat'];
 $nomeCategoria = $_POST['nomeCategoria'];
 //Altera os dados da categoria
+
+$consulta = "SELECT nomeCategoria FROM categoria WHERE nomeCategoria = '$nomeCategoria'";
+
+$consultaresul = mysqli_query($conexao, $consulta);
+
+$resultadoConsulta = mysqli_num_rows($consultaresul);
+
+if($resultadoConsulta > 0){
+    echo '<script type="text/javascript">
+            alert("Erro ao alterar, categoria já consta no banco de dados!");
+            window.history.go(-1);
+          </script>';
+    exit;
+}
+
 $sql="UPDATE categoria set idCat = '$idCat', nomeCategoria='$nomeCategoria' where idCat = $idCat";
 
 $result = mysqli_query($conexao,$sql);
